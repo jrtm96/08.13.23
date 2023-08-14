5ce22ac9115cd27040a42307b20340c10090e728
jrtm96 <102900240+jrtm96@users.noreply.github.com>
2023-08-13 22:24:04 -0500

    msde some js tests

---
+++ main.js
@@ -0,0 +1,15 @@
+const monthlyRent = 500;
+
+const yearlyRent = monthlyRent * 12;
+
+console.log(yearlyRent);
+
+console.log("Hello from the other siiiiide");
+
+
+
+const firstName = "Jose";
+
+const madeUpLastName = "Rosa";
+
+console.log(`Hello, World! My name is  ${firstName} ${madeUpLastName}, Nice to meet you!`);
